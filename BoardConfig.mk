d56f3b33df8cc9ebfed509d55a356fa2c7ed1349
Harsh Shandilya <msfjarvis@gmail.com>
2017-03-11 17:35:00 +0530

    jalebi : Fix time reporting in recovery
    Change-Id: I39f5777d2ad371d0010606c6849cdd5bcce3bda3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,3 +50,4 @@ RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_MAX_BRIGHTNESS := 255
 TW_THEME := portrait_hdpi
+TARGET_RECOVERY_QCOM_RTC_FIX := true
